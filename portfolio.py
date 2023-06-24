d65f88f0f4713d88653195af2c363e36127f2c16
Luca Mancusi <luca.mancusi@protonmail.ch>
2023-06-24 11:24:25 +0000

    Partial refactor of portfolio.py, generalize test_portfolio.py test case using pickles

--- portfolio.py
+++ portfolio.py
@@ -2,60 +2,6 @@ import pandas as pd
 
 
 class Portfolio:
-    def __init__(
-        self,
-        portfolio_file: str = 'portfolio.csv',
-        allocation_file: str = 'allocation.csv',
-        currency: str = 'EUR',
-    ):
-        self._pf = Portfolio._read_portfolio(portfolio_file)
-        self._al = Portfolio._read_allocation(allocation_file)
-        self._currency = currency
-
-    def __str__(self):
-        return self.summary.to_string()
-
-    def __repr__(self):
-        return f'Portfolio(currency={self.currency}, total_value={self.total_value})'
-
-    @property
-    def currency(self) -> str:
-        return self._currency
-
-    @currency.setter
-    def currency(self, currency: str) -> None:
-        self._currency = currency
-
-    @property
-    def total_value(self) -> float:
-        try:
-            return self._pf['Current Value'].sum()
-        except KeyError as e:
-            print(e)
-            raise KeyError(e) from None
-
-    @property
-    def summary(self) -> pd.DataFrame:
-        df = self._pf.drop(['Amount', 'Closing', 'Local Value'], axis=1)
-        df['Current Percentage'] = (df['Current Value'] / self.total_value * 100).apply(
-            lambda x: round(x, 2)
-        )
-
-        df = df.join(self._al)
-        df['Expected Value'] = (
-            self.total_value / 100 * df['Expected Percentage']
-        ).round(2)
-
-        return df[
-            [
-                'Product',
-                'Current Value',
-                'Expected Value',
-                'Current Percentage',
-                'Expected Percentage',
-            ]
-        ]
-
     @staticmethod
     def _read_file(file: str) -> pd.DataFrame:
         try:
@@ -65,61 +11,9 @@ class Portfolio:
             raise FileNotFoundError(e) from None
 
     @staticmethod
-    def _read_portfolio(portfolio_file: str) -> pd.DataFrame:
-        df = Portfolio._read_file(portfolio_file)
-        Portfolio._clean_portfolio(df)
-
-        return df
+    def _replace_columns(df: pd.DataFrame) -> pd.DataFrame:
+        raise NotImplementedError
 
     @staticmethod
-    def _clean_portfolio(df: pd.DataFrame) -> None:
-        try:
-            df.columns = [
-                'Product',
-                'ISIN',
-                'Amount',
-                'Closing',
-                'Local Value',
-                'Current Value',
-            ]
-        except ValueError as e:
-            print(e)
-            raise ValueError(e) from None
-
-        try:
-            df.dropna(subset=['ISIN'], inplace=True)
-        except KeyError as e:
-            print(e)
-            raise KeyError(e) from None
-
-        df.set_index('ISIN', inplace=True)
-
-        for col in ('Current Value', 'Closing'):
-            if all(isinstance(item, str) for item in df[col]):
-                try:
-                    df[col] = (
-                        df[col]
-                        .apply(lambda x: x.replace(',', '.'))
-                        .astype('float')
-                    )
-                except (KeyError, AttributeError, ValueError) as e:
-                    print(e)
-                    raise type(e)(e) from None
-
-
-    @staticmethod
-    def _read_allocation(allocation_file: str) -> pd.DataFrame:
-        df = Portfolio._read_file(allocation_file)
-
-        try:
-            if (s := df['Expected Percentage'].sum()) != 100:
-                raise ValueError(
-                    f'The total sum of percentages in the "Percentage" column is {s}%, not 100.0%'
-                )
-        except KeyError as e:
-            print(e)
-            raise KeyError(e) from None
-
-        df.set_index('ISIN', inplace=True)
-
-        return df
+    def _dropna_isin(df: pd.DataFrame) -> pd.DataFrame:
+        return df.dropna(subset=['ISIN'])
--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -1,20 +1,63 @@
-from sys import path
+import sys
+from pathlib import Path
 
 import pandas as pd
 import pytest
 
-path.append('..')
+project_dir = Path(__file__).resolve().parents[1]
+sys.path.append(str(project_dir))
 
 from portfolio import Portfolio
 
-@pytest.mark.parametrize('file', ['portfolio_EUR.csv', 'portfolio_GBP.csv'])
-def test_read_file_well_formed(file):
-    df = Portfolio._read_file(file)
-    expected_df = pd.read_csv(file)
+portfolios_csv = ['portfolio_EUR.csv', 'portfolio_GBP.csv']
+portfolios_plane = ['portfolio_EUR.pickle', 'portfolio_GBP.pickle']
+portfolios_dropna = ['portfolio_EUR_dropna.pickle', 'portfolio_GBP_dropna.pickle']
+portfolios_columns = ['portfolio_EUR_columns.pickle', 'portfolio_GBP_columns.pickle']
 
-    assert isinstance(df, pd.DataFrame)
-    assert df.equals(expected_df)
+
+@pytest.fixture
+def df_expected_from_pickle(request):
+    return pd.read_pickle(project_dir / 'tests' / 'pickles' / request.param)
+
+
+@pytest.fixture
+def df_working_from_pickle(request):
+    return pd.read_pickle(project_dir / 'tests' / 'pickles' / request.param)
+
+
+@pytest.fixture
+def raw_csv_portfolio(request):
+    return project_dir / 'tests' / 'csv' / request.param
+
+
+@pytest.mark.parametrize(
+    'df_expected_from_pickle, raw_csv_portfolio',
+    zip(portfolios_plane, portfolios_csv),
+    indirect=True,
+)
+def test_read_file(df_expected_from_pickle, raw_csv_portfolio):
+    df = Portfolio._read_file(raw_csv_portfolio)
+
+    assert df.equals(df_expected_from_pickle)
+
 
 def test_read_file_raise_FileNotFoundError():
     with pytest.raises(FileNotFoundError):
-        df = Portfolio._read_file('portfolio.csv')
+        Portfolio._read_file(project_dir / 'tests' / 'csv' / 'missing.csv')
+
+
+@pytest.mark.parametrize('df_working_from_pickle', portfolios_plane, indirect=True)
+def test_replace_columns_raise_NotImplementedError(df_working_from_pickle):
+    with pytest.raises(NotImplementedError):
+        Portfolio._replace_columns(df_working_from_pickle)
+
+
+@pytest.mark.parametrize(
+    'df_expected_from_pickle, df_working_from_pickle',
+    zip(portfolios_dropna, portfolios_columns),
+    indirect=True,
+)
+def test_dropna_isin(df_expected_from_pickle, df_working_from_pickle):
+    assert df_expected_from_pickle.equals(
+        Portfolio._dropna_isin(df_working_from_pickle)
+    )
